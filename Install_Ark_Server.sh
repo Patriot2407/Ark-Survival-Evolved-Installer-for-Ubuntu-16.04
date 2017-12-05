b68ca6f256c5c9d0fa8ed45f26dcee931d70ed3f
czebiniak <30630518+czebiniak@users.noreply.github.com>
2017-12-05 17:01:29 -0500

    Ver 1.0
    Script is born

---
+++ Install_Ark_Server.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo add-apt-repository multiverse 
+sudo apt-get update
+sudo apt-get update
+sudo apt-get install steamcmd -y
+echo "fs.file-max=100000" >> /etc/sysctl.conf && sysctl -p
+echo "* soft nofile 1000000" >> /etc/security/limits.conf
+echo "* hard nofile 1000000" >> /etc/security/limits.conf
+echo "session required pam_limits.so" >> /etc/pam.d/common-session
+if [ -d "/Ark" ]
+	then
+	echo "Ark directory already exists. Continuing on..."
+	sudo chmod 777 /Ark
+	else
+	sudo mkdir /Ark
+	sudo chmod -R 777 /Ark
+	fi
+cd /Ark
+sudo ln -s /usr/games/steamcmd steamcmd
+steamcmd +login anonymous +force_install_dir /Ark +app_update 376030 +quit
+exit 0
