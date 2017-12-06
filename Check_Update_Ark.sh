b21c64db0714ceb4be678269ed74299b220c42ea
czebiniak <30630518+czebiniak@users.noreply.github.com>
2017-12-06 09:41:25 -0500

    Ver 1.0
    Just some cleaning up, and a few changes to make things easier for people. Also removed a script that is used for diagnotic purposes only, and not really useful for anyone else.

--- Check_Update_Ark.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin	#Sets the PATH for CRON
-# /usr/games/steamcmd +login anonymous +app_info_update 1 +app_info_print 376030 +quit # don't need, to much info
-/usr/games/steamcmd +login anonymous +app_info_update 376030 +quit
-exit 0
--- Install_Ark_Server.sh
+++ Install_Ark_Server.sh
@@ -18,4 +18,9 @@ if [ -d "/Ark" ]
 cd /Ark
 sudo ln -s /usr/games/steamcmd steamcmd
 steamcmd +login anonymous +force_install_dir /Ark +app_update 376030 +quit
+echo ""
+echo -n "Please modify the ark.service file to fit your needs.Sessioname needs to be whatever you want to call your server, and others will see on the game. Change EXAMPLE to what you want. To change game map, modify the part that says Ragnarok? and chnage it to whatever map you want like TheIsland?"
+read
+sudo cp ark.service /lib/systemd/system/
+sudo systemctl daemon-reload
 exit 0
